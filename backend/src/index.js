ce7b2a139eea76c5f0f6d309d7765ab9d1d122fd
Ottavia <ottavia.belotti@mail.polimi.it>
2022-12-08 17:42:57 +0100

    Add: (almost) complete FSM state transitions

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const { dockStart } = require('@nlpjs/basic');
+const { GreenHomeComponent } = require('./utils/GreenHomeComponent')
 
 async function compute(userInput) {
     const dock = await dockStart();
@@ -9,52 +10,128 @@ async function compute(userInput) {
     return response.answer;
 }
 
-function fsmProcessState(intent) {
+function fsmProcessState(intent, fsm) {
     console.log(intent)
+
+    // mega switch case for meaningful intent that cause to switch in the fsm --> set actionName to send to dispatch
+    //actionName = 'getOffender'
+    //fsm.dispatch(actionName)
 }
 
 const FSM =  {
     state: 'RESET',
+    currentDevice: null, //extra info to track the device the user is talking about (?)
     transitions: {
         RESET : {
             getOffender() {
                 this.state = 'OFFENDER'
-
+                this.currentDevice = new GreenHomeComponent().offender.name
             },
-            askLeafyLevel() {
+            getGenericTip() {
+                this.state = 'GENERIC_TIP'
+            },
+            howOffenderIsChosen() {
+                this.state = 'OFFENDER_INFO'
+            },
+            getSpecificTip(device) {
+                this.currentDevice = device
+                this.state = 'DEVICE_TIP'
+            },
+            getLeafyStatus() {
                 this.state = 'LEAFY_STATUS'
+            },
+            getLeafyProgress() {
+                this.state = 'LEAFY_PROGRESS'
+            },
+            getTodayConsumption() {
+                this.state = 'TODAY_CONSUMPTION'
             }
         },
         OFFENDER: {
-            approve() {
-                this.status = 'OFFENDER_TIP'
-            },
-            negate() {
-                this.status = 'RESET'
+            getOffenderTip () {
+                this.state = 'WAIT_APPROVAL_FOR_SPECIFIC_TIP'
             }
         },
-        OFFENDER_HISTORY: {
-
-        },
-        OFFENDER_TIP: {
-            giveOffenderTip() {
+        OFFENDER_INFO: {
+            getGenericTip() {
+                this.state = 'WAIT_APPROVAL_FOR_GENERIC_TIP'
             }
         },
         DEVICE_TIP: {
-
+            getSpecificTip() {
+                this.state = 'WAIT_APPROVAL_FOR_SPECIFIC_TIP'
+            }
         },
         GENERIC_TIP: {
-
+            getGenericTip() {
+                this.state = 'WAIT_APPROVAL_FOR_GENERIC_TIP'
+            }
         },
         LEAFY_STATUS: {
-
+            proposeLeafyExplaination() {
+                this.state = 'WAIT_APPROVAL_LEAFY_INFO'
+            },
+            negateOrDefault() {
+                this.state = 'RESET'
+            }
+        },
+        WAIT_APPROVAL_FOR_SPECIFIC_TIP: {
+            approve() {
+                this.state = 'DEVICE_TIP'
+            },
+            negateOrDefault() {
+                this.state = 'RESET'
+            }
+        },
+        WAIT_APPROVAL_FOR_GENERIC_TIP: {
+            approve() {
+                this.state = 'GENERIC_TIP'
+            },
+            negateOrDefault() {
+                this.state = 'RESET'
+            }
+        },
+        WAIT_APPROVAL_LEAFY_INFO: {
+            approve() {
+                this.state = 'LEAFY_INFO'
+            },
+            negateOrDefault() {
+                this.state = 'RESET'
+            }
+        },
+        LEAFY_INFO: {
+            negateOrDefault() {
+                this.state = 'RESET'
+            }
+        },
+        LEAFY_PROGRESS: {
+            negateOrDefault() {
+                this.state = 'RESET'
+            }
+        },
+        TODAY_CONSUMPTION: {
+            approve() {
+                this.state = 'CONSUMPTION_HISTORY_DETAIL'
+            },
+            negateOrDefault() {
+                this.state = 'RESET'
+            }
+        },
+        CONSUMPTION_HISTORY_DETAIL: {
+            negateOrDefault() {
+                this.state = 'RESET'
+            }
         }
     },
-    dispatch(actionName) {
+    dispatch(actionName, device=null) {
         const action = this.transitions[this.state][actionName];
 
         if (action) {
-            action.call(this);
+            if (device != null) {
+                action.call(this, device)
+            } else {
+                action.call(this);
+            }
         } else {
             console.log('invalid action')
             this.state = 'RESET'
